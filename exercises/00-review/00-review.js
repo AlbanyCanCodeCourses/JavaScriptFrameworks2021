ee94b007d1637c5163e868297b0d280956705154
Louis345 <jt125845@gmail.com>
2021-02-21 13:52:43 -0500

    updated tests

--- exercises/00-review/00-review.js
+++ exercises/00-review/00-review.js
@@ -1,5 +1,5 @@
 /**
- * Loop through the array using a for loop (or for ... of loop) and return the highest number
+ *  Return the highest number in the array
  * @param  { array }
  * @returns { number } the highest number that was in the array
  * @example
@@ -7,6 +7,7 @@
  * highestNumber([-1, -5, -4]) // -1
  *
  **/
+
 function highestNumber(array) {}
 
 /**
@@ -19,29 +20,6 @@ function highestNumber(array) {}
  **/
 
 function combineArray(array1, array2) {}
-
-/***
- * Use sets to remove duplicate elements from an array
- * @return {array}
- */
-
-function removeDuplicates() {
-  let numbers = [2, 3, 4, 4, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 5, 32, 3, 4, 5]; // You can change this line
-
-  /** Return the an array of unique values */
-  return;
-}
-
-function createStudentObject() {
-  // Do not change student here
-  let student = {
-    firstName: "",
-    lastName: "",
-    skills: [],
-  };
-  // Add code here
-  return student;
-}
 
 /**
  * Combine two objects into one
@@ -93,37 +71,39 @@ function removeVowels(str) {}
 
 /**
  *  https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Conditional_Operator
- * 
- * 
- * Refactor This  If Else Statement to use the ternary operator
- *      let hungry = true;
-        if (hungry) {
-           console.log('Go eat something.');
-          } else {
-            console.log('Keep coding!');
-         }
- */
-
-function isHungry() {}
-
-/***
- *  Use destructing to solve this problem
- *  @example https://slides.com/accjavascript/deck-10#/41
- *
- *  Function should return tomorrow's temperature
+ * @returns {string}
  *
  */
 
-const AVG_TEMPERATURES = {
-  today: 77.5,
-  tomorrow: 79,
-};
+function getIsHungryText() {
+  /* convert this if else statement into a ternary expression */
+  let isHungry = "";
+  if (hungry) {
+    isHungry = "Go eat something.";
+  } else {
+    isHungry = "Keep coding!";
+  }
+  return isHungry;
+}
 
-function getTempOfTmrw(avgTemperatures) {
-  // change code below this line;
-  const {} = AVG_TEMPERATURES; // change this line
-  // change code above this line
-  return tempOfTomorrow;
+/**
+ * Refactor this to use destructuring
+ * @example https://slides.com/accjavascript/deck-10#/41
+ * @return Function should return tomorrow's temperature
+ */
+
+function getTempOfTmrw() {
+  // Do not change me
+  const AVG_TEMPERATURES = {
+    today: 77.5,
+    tomorrow: 79,
+  };
+
+  // Start of what you should change
+  const today = AVG_TEMPERATURES.today;
+  const tomorrow = AVG_TEMPERATURES.tomorrow;
+  // End of what you should change
+  return `Today's temperature is ${today}.\nTomorrow's temperature is ${tomorrow}`;
 }
 
 /**
@@ -136,3 +116,15 @@ function getTempOfTmrw(avgTemperatures) {
  *  addItems([1,-2,-3]) // -4
  */
 function addItems(arr) {}
+
+/***
+ * Use sets to remove duplicate elements from an array
+ * @return {array}
+ */
+
+function removeDuplicates() {
+  let numbers = [2, 3, 4, 4, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 5, 32, 3, 4, 5]; // You can change this line
+
+  /** Return the an array of unique values */
+  return;
+}
