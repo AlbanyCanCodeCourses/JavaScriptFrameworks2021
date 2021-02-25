bf517ec1224d3feb0f1b30b62ef62b99478eb874
Godson Umoren <umorenkaykay@gmail.com>
2021-02-24 22:03:47 -0500

    :wrench:

--- exercises/01-arrays/src/App.jsx
+++ exercises/01-arrays/src/App.jsx
@@ -1,8 +1,18 @@
 import "./App.css";
 // import something here
+import images from "./images/images";
 
 function App() {
-  return <div className="App">{/* Complete me */}</div>;
+  return (
+    <div className="App">
+      {images.map((child, index) => (
+        <>
+          <img src={child} alt="" className="img" />
+          {/*<text>{index}</text>*/}
+        </>
+      ))}
+    </div>
+  );
 }
 
 export default App;
