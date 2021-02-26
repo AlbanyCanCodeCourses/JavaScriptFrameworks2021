d6805a59a7d818021324cd73d7fa35f9f64bd868
mjones12209 <m.jones12209@gmail.com>
2021-02-26 00:50:41 -0500

    Revert "completed 01-arrays and 01-props"
    This reverts commit 536b730d42c4f4f1b6dc67cb9a2a2282b5589222, reversing
    changes made to 78547d96fb0dae9ab572100b5c201c6ea85b32f7.

---
+++ exercises/02-arrays/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import React from 'react';
+import Images from './images/images';
+
+const App = () => {
+  return (
+  <div className="App">
+    <Images />
+    </div>
+    );
+};
+
+export default App;
