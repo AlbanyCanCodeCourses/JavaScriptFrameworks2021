a10704581643ae74de743afb5e7715a9dd0dbbd3
Sean Russell <seanrussellphotography@gmail.com>
2021-03-01 20:42:37 -0500

    hw1

--- exercises/01-arrays/src/App.jsx
+++ exercises/01-arrays/src/App.jsx
@@ -1,8 +1,13 @@
 import "./App.css";
+import { images } from './images/images.js';
 // import something here
 
 function App() {
-  return <div className="App">{/* Complete me */}</div>;
+  return (
+  <div className="App">
+    {images.map(image => <img key={image} alt='Array of images' src={image}/>)}
+  </div>
+  );
 }
 
 export default App;
