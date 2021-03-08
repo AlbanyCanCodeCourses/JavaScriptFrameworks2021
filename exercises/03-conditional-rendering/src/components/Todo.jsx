23527435e0ccbb47acc97262df354b1845c99dc5
Sean Russell <seanrussellphotography@gmail.com>
2021-03-08 18:49:18 -0500

    todos

--- exercises/03-conditional-rendering/src/components/Todo.jsx
+++ exercises/03-conditional-rendering/src/components/Todo.jsx
@@ -1,16 +1,12 @@
 const Todo = ({ todos, showCompleted }) => {
-  // Take one todo and return an element
-  const showTodo = todo => <h4 key={todo.id}>{todo.title}</h4>
   // Take an array of todos and return array of elements
-  const showTodos = myTodos => myTodos.map(todo => showTodo(todo))
+  const showTodos = myTodos => myTodos.map(todo => <li key={todo.id}>{todo.title}</li>)
 
   return (
-    <div>
-
+    <ul>
       {showCompleted ? showTodos(todos)
         : showTodos(todos.filter(todo => !todo.completed))}
-
-    </div>
+    </ul>
   );
 }
 
