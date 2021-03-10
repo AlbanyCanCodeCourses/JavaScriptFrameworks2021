e8945deea6ac2a6408d878b80b6448e1b5deceb4
Godson Umoren <umorenkaykay@gmail.com>
2021-03-10 10:24:21 -0500

    HW 6 (1 & 4) Completed

---
+++ exercises/06-grocery-list/src/Components/Journal.js
@@ -0,0 +1,65 @@
+{
+  /* <table className="table table-sm">
+          <thead>
+            <tr>
+              <th>Item</th>
+              <th>Cost</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody className="table">
+            {/**
+             * Complete me. (You can use something else instead of a table if you like)
+             * @example
+             * <tr>
+             *   <td>Toilet Paper</td>
+             *   <td>$1.99</td>
+             *   <td>
+             *     <button aria-label="Delete" title="Delete" ... >
+             *       &times;
+             *     </button>
+             *   </td>
+             * </tr>
+             */
+}
+
+{
+  /* {items.map((subItems, idx) => {
+              return (
+                <div className="cell" key={idx}>
+                  {subItems}
+                </div>
+              );
+            })}
+
+            {costs.map((subCosts, idx) => {
+              return (
+                <div className="cell" key={idx}>
+                  {subCosts}
+                </div>
+              );
+            })} */
+}
+{
+  /* {combinedArray.map((items, idx) => {
+                return (
+                  <>
+                    <td key={idx}>{items}</td>
+                    <td>
+                      <button aria-label="Delete" title="Delete">
+                        &times;
+                      </button>
+                    </td>
+                  </>
+                );
+              })} */
+}
+{
+  /* <td></td> */
+}
+{
+  /* </tbody> */
+}
+{
+  /* </table> */
+}
