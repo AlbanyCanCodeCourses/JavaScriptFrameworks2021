94d8c0c194080d1a89b3e666377f22b26dd4ee2e
Matina <matinaspatsos@gmail.com>
2021-03-24 20:11:26 -0400

    Touching up on the second useEffect exercise

--- exercises/09b-use-effect/src/App.jsx
+++ exercises/09b-use-effect/src/App.jsx
@@ -26,24 +26,23 @@ function App() {
   return (
     <div className="App">
       <h1>Dogs</h1>
-      {/* Attach an event handler */}
+      {/* Make me a controlled input */}
       <select>
-        <option>1</option>
-        <option>2</option>
-        <option>3</option>
-        <option>4</option>
-        <option>5</option>
-        <option>6</option>
-        <option>7</option>
-        <option>8</option>
-        <option>9</option>
-        <option>10</option>
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+        <option value="5">5</option>
+        <option value="6">6</option>
+        <option value="7">7</option>
+        <option value="8">8</option>
+        <option value="9">9</option>
+        <option value="10">10</option>
       </select>
       <div className="container">
-        {dogImages &&
-          dogImages.map((dogImage, idx) => {
-            return <img key={`dog-${idx}`} height="200" src={dogImage} />;
-          })}
+        {dogImages.map((dogImage, idx) => {
+          return <img key={`dog-${idx}`} height="200" src={dogImage} />;
+        })}
       </div>
     </div>
   );
