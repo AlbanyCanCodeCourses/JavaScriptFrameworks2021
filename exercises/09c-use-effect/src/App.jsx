7164c45a848f2a5ee5d41ebfca61e8a9439010e5
mjones12209 <m.jones12209@gmail.com>
2021-03-29 18:10:58 -0400

    tech issues

---
+++ exercises/09c-use-effect/src/App.jsx
@@ -0,0 +1,41 @@
+// import useEffect
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  useEffect(
+    ()=> {
+      const getData = async() => {
+
+      }
+    }
+  );
+
+  return (
+    <div className="container">
+      <div className="row text-center" id="body">
+        <h1 id="title-head">{/* Plugin character name here */}</h1>
+        <div id="main-img">
+          <a href="http://rickandmorty.wikia.com/wiki/Rick_Sanchez">
+            {/* Add an alt and src to this image */}
+            <img height="250" />
+          </a>
+          <div className="linkfooter">
+            <p>Select your favorite character</p>
+            {/* Handle event here */}
+            <select id="dropdown" type="text">
+              <option></option>
+              {/**
+               * Loop through all characters. The value should be the character id.
+               * @example in HTML
+               * <option value="2" key="character-1">Morty Smith</option>
+               */}
+            </select>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
