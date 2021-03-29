15affb983b26309ad58960d73f601907c9f28daa
mjones12209 <m.jones12209@gmail.com>
2021-03-29 19:17:47 -0400

    finished exercise 09c-use-effect

--- exercises/09c-use-effect/src/App.jsx
+++ exercises/09c-use-effect/src/App.jsx
@@ -1,35 +1,41 @@
-// import useEffect
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
+import axios from 'axios';
 
 function App() {
+  const [characters, setCharacters] = useState();
+  const [character, setCharacter] = useState();
+  const [options, setOptions] = useState();
+
   useEffect(
     ()=> {
-      const getData = async() => {
-
+      const getCharacters = async() => {
+        setCharacters(await axios("https://rickandmortyapi.com/api/character"));
       }
+      getCharacters()
     }
-  );
+  , []);
 
   return (
     <div className="container">
       <div className="row text-center" id="body">
-        <h1 id="title-head">{/* Plugin character name here */}</h1>
+        <h1 id="title-head">{character && character[0].name}</h1>
         <div id="main-img">
           <a href="http://rickandmorty.wikia.com/wiki/Rick_Sanchez">
             {/* Add an alt and src to this image */}
-            <img height="250" />
+            <img height="250" src={character && character[0].image} />
           </a>
           <div className="linkfooter">
             <p>Select your favorite character</p>
             {/* Handle event here */}
-            <select id="dropdown" type="text">
-              <option></option>
-              {/**
-               * Loop through all characters. The value should be the character id.
-               * @example in HTML
-               * <option value="2" key="character-1">Morty Smith</option>
-               */}
+            <select id="dropdown" type="text" onChange={(e)=> {
+              setCharacter(characters.data.results.filter((character)=> {
+                return character.name === e.target.value
+              }))
+            }}>
+              {characters && characters.data.results.map((character, index)=> {
+                return <option key={`char-${index}`}>{character.name}</option>
+              })}
             </select>
           </div>
         </div>
