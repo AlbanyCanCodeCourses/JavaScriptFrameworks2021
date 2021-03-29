48994a8687529c5c489700447599fdb5be52cd38
Sean Russell <seanrussellphotography@gmail.com>
2021-03-29 17:42:55 -0400

    exercise 9a

--- exercises/09a-use-effect/src/App.jsx
+++ exercises/09a-use-effect/src/App.jsx
@@ -1,12 +1,26 @@
 // Import useEffect here
 import "./App.css";
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 import ronSwansonImage from "./assests/ronswanson.png";
 // import Axios (or use Fetch)
 
 function App() {
-  /**
-   * Set up your state
-   */
+  const [quote, setQuote] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [hasError, setHasError] = useState(false)
+
+  const SWANSON_API = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+
+  useEffect(() => {
+    setQuote("");
+    setIsLoading(true);
+    axios.get(SWANSON_API)
+      .then(response => response.data)
+      .then(newQuote => setQuote(newQuote))
+      .catch(() => setHasError(true))
+      .then(() => setIsLoading(false))
+  }, []);
 
   /**
    * Make an AJAX call with the useEffect hook
@@ -16,13 +30,13 @@ function App() {
     <body class="bg-warning text-center">
       <img src={ronSwansonImage} alt="" width="450" height="423" class="mt-4" />
       <div class="container">
-        {/* Display a loading message */}
-        {/* Display an error message if the API fails */}
+        {isLoading && <h2>Loading a quote...</h2>}
+        {hasError && <h2>There was a problem loading the quote!!</h2>}
         <blockquote
           id="quote"
           class="blockquote bg-dark text-white border-0 mb-4"
         >
-          {/* Complete me */}
+          {quote}
         </blockquote>
       </div>
     </body>
