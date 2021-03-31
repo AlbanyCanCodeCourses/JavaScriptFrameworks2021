8359573d848ab1f63176513466a8a05af0448d5c
Sean Russell <seanrussellphotography@gmail.com>
2021-03-31 18:22:45 -0400

    exercise 09c-use-effect

--- exercises/09c-use-effect/src/App.jsx
+++ exercises/09c-use-effect/src/App.jsx
@@ -1,32 +1,47 @@
-// import useEffect
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from 'axios';
 import "./App.css";
-// import Axios (or use Fetch)
 
 function App() {
-  /**
-   * Set up state and make AJAX requests here
-   */
+
+  const [selectedCharacter, setSelectedCharacter] = useState({})
+  const [allCharacters, setAllCharacters] = useState([]);
+
+  const CHARACTER_API_URL = 'https://rickandmortyapi.com/api/character/';
+
+  useEffect(() => {
+    axios.get(CHARACTER_API_URL+'?page=4')
+      .then(results => results.data)
+      .then(data => setAllCharacters(data.results))
+      .catch(error => console.log(error))
+  }, [])
+
+  const handleCharacterSelect = e => {
+    e.preventDefault();
+    // Set selectedCharacter to character object with matching id
+    const characterID = parseInt(e.target.value);
+    characterID 
+      ? setSelectedCharacter(allCharacters.find(character => character.id === characterID))
+      : setSelectedCharacter({});
+  }
+  console.log(selectedCharacter);
+  console.log(allCharacters);
 
   return (
     <div className="container">
       <div className="row text-center" id="body">
-        <h1 id="title-head">{/* Plugin character name here */}</h1>
+        <h1 id="title-head">{selectedCharacter.name || ''}</h1>
         <div id="main-img">
           <a href="http://rickandmorty.wikia.com/wiki/Rick_Sanchez">
             {/* Add an alt and src to this image */}
-            <img height="250" />
+            {selectedCharacter !== {} && <img height="250" alt={selectedCharacter.name} src={selectedCharacter.image} />}
           </a>
           <div className="linkfooter">
             <p>Select your favorite character</p>
             {/* Handle event here */}
-            <select id="dropdown" type="text">
-              <option></option>
-              {/**
-               * Loop through all characters. The value should be the character id.
-               * @example in HTML
-               * <option value="2" key="character-1">Morty Smith</option>
-               */}
+            <select id="dropdown" type="text" value={selectedCharacter.id} onChange={e => handleCharacterSelect(e)}>
+              <option value={0}></option> 
+              {allCharacters.map(character => <option value={character.id} key={'character-'+character.id}>{character.name}</option> )}
             </select>
           </div>
         </div>
