814738bebadf4301e8e58b250f3a8e85f3cd7ad4
JFMGitHub <jmellet1@binghamton.edu>
2021-03-31 17:42:46 -0400

    09c

--- exercises/09c-use-effect/src/App.jsx
+++ exercises/09c-use-effect/src/App.jsx
@@ -1,27 +1,45 @@
-// import useEffect
+import {useEffect} from "react"// import useEffect
 import { useState } from "react";
 import "./App.css";
-// import Axios (or use Fetch)
+import axios from "axios"// import Axios (or use Fetch)
 
 function App() {
   /**
    * Set up state and make AJAX requests here
    */
+  const [charName, setCharName] = useState([]);
+  const [charImage, setCharImage] = useState([]);
 
+  useEffect(() => {
+    axios(`https://rickandmortyapi.com/api/character`)
+    .then(response => setCharName(response.data.results))
+  },[])
+
+  const characterNames = [];
+  const characterImages = [];
   return (
     <div className="container">
       <div className="row text-center" id="body">
         <h1 id="title-head">{/* Plugin character name here */}</h1>
         <div id="main-img">
           <a href="http://rickandmorty.wikia.com/wiki/Rick_Sanchez">
-            {/* Add an alt and src to this image */}
-            <img height="250" />
+            
+            <img height="250" src={charImage} />
           </a>
           <div className="linkfooter">
             <p>Select your favorite character</p>
-            {/* Handle event here */}
-            <select id="dropdown" type="text">
-              <option></option>
+            {charName.forEach((element) => characterNames.push(element.name))}
+            {charName.forEach((element) => characterImages.push(element.image))}
+            {console.log(characterNames)}
+            {console.log(characterImages)}
+            <select id="dropdown" type="text" onChange={(event) => setCharImage(characterImages[event.target.value])}>
+              {characterNames.map((x, index) => { return(
+              <option value={index}>{x}</option>
+              )
+              }
+              )}
+              
+              
               {/**
                * Loop through all characters. The value should be the character id.
                * @example in HTML
