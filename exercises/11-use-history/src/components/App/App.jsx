4c75f58f5e07437f6ca97e05f36f8050dc65d87c
Sean Russell <seanrussellphotography@gmail.com>
2021-04-05 19:49:37 -0400

    exercise 11

--- exercises/11-use-history/src/components/App/App.jsx
+++ exercises/11-use-history/src/components/App/App.jsx
@@ -1,11 +1,27 @@
 // Import things from React Router
 import Subscribe from "../Subscribe/Subscribe";
 import ThankYou from "../ThankYou/ThankYou";
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom'
 
 function App() {
   return (
+
     <div className="uk-height-1-1 uk-flex uk-flex-column uk-flex-center">
-      {/* Complete me */}
+      <Router>
+        <Switch>
+          <Route path="/subscribe">
+            <Subscribe/>
+          </Route>
+          <Route path="/thank-you">
+            <ThankYou/>
+          </Route>
+        </Switch>
+        <Switch>
+          <Route path="/">
+            <Redirect to="/subscribe"/>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
