888c5830929b7adf8025b724c9d593c5ee28a174
mjones12209 <m.jones12209@gmail.com>
2021-04-09 02:14:24 -0400

    completed exercise 12a

--- exercises/12a-authentication/src/components/App/App.jsx
+++ exercises/12a-authentication/src/components/App/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 // import something here
+import Home from '../Home/Home';
 // import Axios (or use Fetch)
+import axios from 'axios';
 
 function App() {
   /**
@@ -14,62 +16,103 @@ function App() {
   const [isLoading, setIsLoading] = useState(false);
   const [errorMessage, setErrorMessage] = useState("");
 
+
   /**
    * Complete all the logging in and logout logic
    */
+  const [token, setToken] = useState(null);
+
+  const logout = () => {
+    setToken(null);
+  }
+
+  const handleSubmit = (e) => {
+    setIsLoading(true);
+    e.preventDefault();
+    const login = async () => {
+      try {
+        const asyncResponse = await axios({
+          method: "POST",
+          url: "http://localhost:7000/api/login",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          data: JSON.stringify({
+            username: username,
+            password: password
+          })
+        });
+          if (asyncResponse.status === 200 && asyncResponse.data.token) {
+            setToken(asyncResponse.data.token);
+            setIsLoading(false);
+          }
+      }
+      catch (error) {
+          if (error.response && error.response.status === 401) {
+            setErrorMessage(`There was an error during login.  Server responded with status code ${error.response.status}`);
+            setIsLoading(false);
+          }
+      }
+    }
+    login();
+  }
 
   /**
    * If the user is logged in, you should render the <Home /> component instead.
    */
-  return (
-    <div className="container mt-2 mb-5">
-      <h1>Login</h1>
-      {/* Handle form submission */}
-      <form className="form-inline mb-2" method="POST">
-        <div className="form-group">
-          <label htmlFor="username" className="mr-2">
-            Username
-          </label>
-          <input
-            type="text"
-            id="username"
-            className="form-control mr-3"
-            required={true}
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="password" className="mr-2">
-            Password
-          </label>
-          <input
-            type="password"
-            id="password"
-            className="form-control mr-3"
-            required={true}
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
-        <button type="submit" className="btn btn-primary" disabled={isLoading}>
-          Login
-        </button>
-      </form>
-      <p className="form-text">
-        <small>
-          The username is <em>username</em> and the password is{" "}
-          <em>password</em>
-        </small>
-      </p>
-      {isLoading && <p>Loading ...</p>}
-      {errorMessage && (
-        <div className="alert alert-danger" role="alert">
-          {errorMessage}
-        </div>
-      )}
-    </div>
-  );
+  if(token) {
+    return <Home token={token} logout={logout} />
+  } else {
+    return (
+      <div className="container mt-2 mb-5">
+        <h1>Login</h1>
+        {/* Handle form submission */}
+        <form className="form-inline mb-2" method="POST" onSubmit={e=>handleSubmit(e)}>
+          <div className="form-group">
+            <label htmlFor="username" className="mr-2">
+              Username
+            </label>
+            <input
+              type="text"
+              id="username"
+              className="form-control mr-3"
+              required={true}
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password" className="mr-2">
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              className="form-control mr-3"
+              required={true}
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <button type="submit" className="btn btn-primary" disabled={isLoading}>
+            Login
+          </button>
+        </form>
+        <p className="form-text">
+          <small>
+            The username is <em>username</em> and the password is{" "}
+            <em>password</em>
+          </small>
+        </p>
+        {isLoading && <p>Loading ...</p>}
+        {errorMessage && (
+          <div className="alert alert-danger" role="alert">
+            {errorMessage}
+          </div>
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
--- exercises/12a-authentication/src/components/Home/Home.jsx
+++ exercises/12a-authentication/src/components/Home/Home.jsx
@@ -1,5 +1,6 @@
 // You might need to import something from React
-import { useState } from "react";
+import axios from "axios";
+import { useState, useEffect } from "react";
 // import Axios (or use Fetch)
 
 function Home({ token, logout }) {
@@ -10,13 +11,33 @@ function Home({ token, logout }) {
    * Make an AJAX request to http://localhost:7000/api/movies to get a list of movies.
    * Be sure to provide the token in the AJAX request.
    */
+  useEffect(()=> {
+    const getMovies = async() => {
+      try {
+        const asyncResponse = await axios({
+          method: "GET",
+          url: "http://localhost:7000/api/movies",
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+        setMovies(asyncResponse.data);
+      }
+      catch (error) {
+        if(error.response && error.response.status === 401){
+          setErrorMessage(`There is a problem with your authentication.  Server responded with status code${error.response.status}`);
+        }
+      }
+    }
+    getMovies();
+  },[token])
 
   return (
     <div className="container mt-2 mb-5">
       <div className="d-flex justify-content-between">
         <h1 className="h2">You are logged in!</h1>
         {/* Make this button functional */}
-        <button className="btn btn-primary">Logout</button>
+        <button className="btn btn-primary" onClick={logout}>Logout</button>
       </div>
       {Object.values(movies).map((movie, idx) => {
         return (
