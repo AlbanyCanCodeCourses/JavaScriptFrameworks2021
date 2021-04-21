b16d83ee23493956204e90f113b9a125c02ef298
Matina <matinaspatsos@gmail.com>
2021-04-21 19:02:59 -0400

    Life cycle hooks assignment

--- exercises/15-component-class/src/components/Likes/Likes.jsx
+++ exercises/15-component-class/src/components/Likes/Likes.jsx
@@ -1,31 +1,3 @@
-// import { Component } from "react";
-
-// class Likes extends Component {
-//   state = {
-//     likes: 0,
-//   };
-
-//   /**
-//    *  Complete this function here to increment the likes counter
-//    *
-//    */
-
-//   like = () => {};
-
-//   render() {
-//     /**
-//      * Make the button functional
-//      */
-//     return (
-//       <div className="mt-4">
-//         <button className="btn btn-light">{this.state.likes} Likes</button>
-//       </div>
-//     );
-//   }
-// }
-
-// export default Likes;
-
 import { Component } from "react";
 
 class Likes extends Component {
@@ -38,18 +10,16 @@ class Likes extends Component {
    *
    */
 
-  like = () => {
-    this.setState({ likes: this.state.likes + 1 });
-  };
+  like = () => {};
 
   render() {
     /**
      * Make the button functional
      */
     return (
-      <button className="btn btn-light" onClick={this.like}>
-        {this.state.likes} Likes
-      </button>
+      <div className="mt-4">
+        <button className="btn btn-light">{this.state.likes} Likes</button>
+      </div>
     );
   }
 }
